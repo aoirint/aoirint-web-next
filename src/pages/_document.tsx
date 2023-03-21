90c040173d730011367630aff1ff923693715c55
aoirint <aoirint@gmail.com>
2023-03-21 16:04:29 +0900

    add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L2PECGJKLB"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-L2PECGJKLB');
+          `
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
