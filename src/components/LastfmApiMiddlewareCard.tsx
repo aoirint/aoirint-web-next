a083c1e950f5bda8e9fb73caae4baa55eb57e2fd
aoirint <aoirint@gmail.com>
2023-04-20 15:06:45 +0900

    lastfm hide if not playing

--- src/components/LastfmApiMiddlewareCard.tsx
+++ src/components/LastfmApiMiddlewareCard.tsx
@@ -55,59 +55,61 @@ const LastfmApiMiddlewareCard: React.FC<{}> = () => {
 
   return (
     <>
-      <div className='card mb-6'>
-        <div className="card-content py-4">
-          <div className="media">
-            <div className="media-left">
-              {isPlaying ? (
-                <a href={lastTrackUrl ?? '#'}>
-                  <figure className="image is-64x64">
-                    <img src={lastTrackAlbumArt != null ? lastTrackAlbumArt?.['#text'] : '#'} alt="Album art" />
+      {isPlaying ? (
+        <div className='card mb-6'>
+          <div className="card-content py-4">
+            <div className="media">
+              <div className="media-left">
+                {isPlaying ? (
+                  <a href={lastTrackUrl ?? '#'}>
+                    <figure className="image is-64x64">
+                      <img src={lastTrackAlbumArt != null ? lastTrackAlbumArt?.['#text'] : '#'} alt="Album art" />
+                    </figure>
+                  </a>
+                ) : loading ? (
+                  <figure className="image is-64x64 has-background-light">
                   </figure>
-                </a>
-              ) : loading ? (
-                <figure className="image is-64x64 has-background-light">
-                </figure>
-              ) : (
-                <figure className="image is-64x64 has-background-light">
-                </figure>
-              )}
-            </div>
-            <div className="media-content">
-              {isPlaying ? (
-                <>
-                  <p className="title is-5">
-                    {'♪ '}
-                    <a href={lastTrackUrl ?? '#'} style={{ color: 'inherit' }}>
-                      {lastTrackTitle}
-                    </a>
-                  </p>
-                  <p className="subtitle is-7 mb-2">
-                    <a href={lastTrackUrl ?? '#'} style={{ color: 'inherit' }}>
-                      {lastTrackAlbumName} - {lastTrackArtist}
-                    </a>
-                  </p>
-                </>
-              ) : loading ? (
-                <>
-                  <p className="title is-5">読み込み中</p>
-                  <p className="subtitle is-7 mb-2">-</p>
-                </>
-              ) : (
-                <>
-                  <p className="title is-5">再生中の音楽はありません</p>
-                  <p className="subtitle is-7 mb-2">-</p>
-                </>
-              )}
-              <p className='is-size-7 has-text-right has-text-grey'>
-                Powered by
-                {' '}
-                <a href="https://www.last.fm/user/aoirint">AudioScrobbler / Last.fm API</a>
-              </p>
+                ) : (
+                  <figure className="image is-64x64 has-background-light">
+                  </figure>
+                )}
+              </div>
+              <div className="media-content">
+                {isPlaying ? (
+                  <>
+                    <p className="title is-5">
+                      {'♪ '}
+                      <a href={lastTrackUrl ?? '#'} style={{ color: 'inherit' }}>
+                        {lastTrackTitle}
+                      </a>
+                    </p>
+                    <p className="subtitle is-7 mb-2">
+                      <a href={lastTrackUrl ?? '#'} style={{ color: 'inherit' }}>
+                        {lastTrackAlbumName} - {lastTrackArtist}
+                      </a>
+                    </p>
+                  </>
+                ) : loading ? (
+                  <>
+                    <p className="title is-5">読み込み中</p>
+                    <p className="subtitle is-7 mb-2">-</p>
+                  </>
+                ) : (
+                  <>
+                    <p className="title is-5">再生中の音楽はありません</p>
+                    <p className="subtitle is-7 mb-2">-</p>
+                  </>
+                )}
+                <p className='is-size-7 has-text-right has-text-grey'>
+                  Powered by
+                  {' '}
+                  <a href="https://www.last.fm/user/aoirint">AudioScrobbler / Last.fm API</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : ''}
     </>
   )
 }
