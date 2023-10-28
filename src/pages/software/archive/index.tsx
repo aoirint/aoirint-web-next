82caa3cb30af10e1cc7f9a052bf54f87695b8069
aoirint <aoirint@gmail.com>
2023-10-28 16:07:12 +0900

    mui: software archive

--- src/pages/software/archive/index.tsx
+++ src/pages/software/archive/index.tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/components/Navbar'
 import SoftwareCardList, { SoftwareCard } from '@/components/SoftwareCardList'
+import { Box, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
-import Link from 'next/link'
+import MuiLink from '@mui/material/Link'
+import NextLink  from 'next/link'
 import React from 'react'
 
 const archiveSoftwareCards: SoftwareCard[] = [
@@ -40,28 +42,30 @@ const ArchiveSoftwares: React.FC<{}> = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <section className='section'>
-        <div className='container'>
-          <h1 className='title'>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <CssBaseline />
+        <Navbar />
+        <Container component="main" sx={{ p: 3, width: '100%' }}>
+          <Toolbar />
+          <Typography variant="h4">
             メンテナンスを終了したソフトウェア
-          </h1>
-          <h2 className='subtitle is-6 mb-4'>
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" gutterBottom>
             開発および保守を終了したソフトウェアを紹介します
-          </h2>
+          </Typography>
           {archiveSoftwareCards.length > 0 ? (
             <SoftwareCardList cards={archiveSoftwareCards} />
           ) : (
-            <p className='content'>
+            <Typography variant="body1">
               現在、項目はありません。
-            </p>
+            </Typography>
           )}
-          <h2 className='title is-4 mt-5'>
+          <Typography variant="h5" sx={{ mt: 3 }}>
             Dockerイメージ
-          </h2>
-          <h3 className='subtitle is-6 mb-4'>
+          </Typography>
+          <Typography variant="subtitle2" color="text.secondary" gutterBottom>
             OSSソフトウェアをDockerイメージ化していました
-          </h3>
+          </Typography>
           {archiveDockerImageCards.length > 0 ? (
             <SoftwareCardList cards={archiveDockerImageCards} />
           ) : (
@@ -69,18 +73,20 @@ const ArchiveSoftwares: React.FC<{}> = () => {
               現在、項目はありません。
             </p>
           )}
-          <h2 className='title is-4 mt-5'>
+          <Typography variant="h5" sx={{ mt: 3 }}>
             その他のソフトウェア
-          </h2>
-          <p className='content'>
-            <ul>
-              <li>
-                <Link href="/software/">ソフトウェア一覧</Link>
-              </li>
-            </ul>
-          </p>
-        </div>
-      </section>
+          </Typography>
+          <ul>
+            <li>
+              <NextLink href="/software/" passHref legacyBehavior>
+                <MuiLink>
+                ソフトウェア一覧
+                </MuiLink>
+              </NextLink>
+            </li>
+          </ul>
+        </Container>
+      </Box>
     </>
   )
 }
