a4663d23d98d96830b3714eec17c0b67e0ffbce3
aoirint <aoirint@gmail.com>
2023-12-24 14:21:44 +0900

    update activity mbed_lcd_runner page to mui

--- src/pages/activity/mbed_lcd_runner/index.tsx
+++ src/pages/activity/mbed_lcd_runner/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import { Box, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
 import React from 'react'
 
@@ -10,35 +11,35 @@ const MbedLcdRunner: React.FC<{}> = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <section className='section'>
-        <div className='container'>
-          <h1 className='title'>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <CssBaseline />
+        <Navbar />
+        <Container component="main" sx={{ p: 3, width: '100%' }}>
+          <Toolbar />
+          <Typography variant="h4">
             mBed LCD Runner
-          </h1>
-          <p className='subtitle'>
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" gutterBottom>
             1ボタンキャラクタディスプレイ障害物避けゲーム
-          </p>
-          <div className='contents'>
-            <p>
-              高校の部活動にて、
-              チーム製作。
-            </p>
-            <p>
-              高校文化祭（2016）にて展示した。
-            </p>
-            <p>
-              mBed、C++、キャラクタディスプレイなどを使用した。
-            </p>
-            <p>
-              開発メンバーとして、主要なプログラムの開発を担当した。
-            </p>
-            <p>
-              開発途中のサルベージ版プログラム：<a href="https://gist.github.com/aoirint/d6bd55c0f4f02582ae7c4069b6486e4c">https://gist.github.com/aoirint/d6bd55c0f4f02582ae7c4069b6486e4c</a>
-            </p>
-          </div>
-        </div>
-      </section>
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            高校の部活動にて、
+            チーム製作。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            高校文化祭（2016）にて展示した。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            mBed、C++、キャラクタディスプレイなどを使用した。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            開発メンバーとして、主要なプログラムの開発を担当した。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            開発途中のサルベージ版プログラム：<a href="https://gist.github.com/aoirint/d6bd55c0f4f02582ae7c4069b6486e4c">https://gist.github.com/aoirint/d6bd55c0f4f02582ae7c4069b6486e4c</a>
+          </Typography>
+        </Container>
+      </Box>
     </>
   )
 }
