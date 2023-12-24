643eed12071a5a33332ae424cbfbff1579697263
aoirint <aoirint@gmail.com>
2023-12-24 14:26:06 +0900

    update activity tabletop_edu page to mui

--- src/pages/activity/tabletop_edu/index.tsx
+++ src/pages/activity/tabletop_edu/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import { Box, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
@@ -12,39 +13,40 @@ const TabletopEdu: React.FC<{}> = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <section className='section'>
-        <div className='container'>
-          <h1 className='title'>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <CssBaseline />
+        <Navbar />
+        <Container component="main" sx={{ p: 3, width: '100%' }}>
+          <Toolbar />
+          <Typography variant="h4">
             テーブルトップディスプレイを活用したグループプログラミング学習ツール
-          </h1>
-          <p className='subtitle'>
-          </p>
-          <div className='contents'>
+          </Typography>
+          <Box sx={{ my: 2 }}>
             <Image
               src="/activity/images/tabletop_edu.jpg"
               alt="Workshop Photo"
               width="800"
               height="600"
             />
-            <p className='mb-4'>
-              （画像は前身となるシステムを利用したプログラミングワークショップの様子）
-            </p>
-            <p>
-              研究開発系サークル<a href="https://imedia-lab.net/">インタラクティブメディアラボ</a>にて、
-              チーム製作。
-            </p>
-            <p>
-              <a href="https://www.uec.ac.jp/news/announcement/2018/20180627_1107.html">UECものづくりコンテスト2018</a>にて、株式会社フローベル賞を受賞。
-            </p>
-            <p>
-              テーブルトップディスプレイシステム PacPac、Unity、Processing、Node.js / express / socket.ioなどを使用した。
-            </p>
-            <p>
-              開発メンバーとして、サーバサイドの開発を担当した。
-            </p>
-          </div>
-        </div>
-      </section>
+          </Box>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            （画像は前身となるシステムを利用したプログラミングワークショップの様子）
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            研究開発系サークル<a href="https://imedia-lab.net/">インタラクティブメディアラボ</a>にて、
+            チーム製作。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            <a href="https://www.uec.ac.jp/news/announcement/2018/20180627_1107.html">UECものづくりコンテスト2018</a>にて、株式会社フローベル賞を受賞。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            テーブルトップディスプレイシステム PacPac、Unity、Processing、Node.js / express / socket.ioなどを使用した。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            開発メンバーとして、サーバサイドの開発を担当した。
+          </Typography>
+        </Container>
+      </Box>
     </>
   )
 }
