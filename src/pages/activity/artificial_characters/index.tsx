1a80200e1574f43458df189f5896236db2214591
aoirint <aoirint@gmail.com>
2023-12-24 14:15:53 +0900

    update activity artificial_characters page to mui

--- src/pages/activity/artificial_characters/index.tsx
+++ src/pages/activity/artificial_characters/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import { Box, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
 import React from 'react'
 
@@ -10,25 +11,23 @@ const ArtificialCharacters: React.FC<{}> = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <section className='section'>
-        <div className='container'>
-          <h1 className='title'>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <CssBaseline />
+        <Navbar />
+        <Container component="main" sx={{ p: 3, width: '100%' }}>
+          <Toolbar />
+          <Typography variant="h4" gutterBottom>
             GANによる人工言語向け架空文字生成
-          </h1>
-          <p className='subtitle'>
-          </p>
-          <div className='contents'>
-            <p>
-              PBL講義<a href="http://pr.cei.uec.ac.jp/kobo2018/index.php">情報工学工房2018 テーマ「Pythonによる深層学習」</a>にて、
-              個人製作。
-            </p>
-            <p>
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
+            PBL講義<a href="http://pr.cei.uec.ac.jp/kobo2018/index.php">情報工学工房2018 テーマ「Pythonによる深層学習」</a>にて、
+            個人製作。
+          </Typography>
+          <Typography variant="body1" sx={{ my: 2 }}>
               PyTorch、DCGANなどを使用した。
-            </p>
-          </div>
-        </div>
-      </section>
+          </Typography>
+        </Container>
+      </Box>
     </>
   )
 }
