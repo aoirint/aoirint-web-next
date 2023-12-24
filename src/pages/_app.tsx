91c002b8971da7c5704ca97661adeb841d80b6ec
aoirint <aoirint@gmail.com>
2023-12-25 01:39:42 +0900

    dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline, createTheme, useMediaQuery } from '@mui/material'
+import type { AppProps } from 'next/app'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  )
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
